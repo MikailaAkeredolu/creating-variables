117749fbfa888cd2b46ce1e7c20b57c14a7b684d
Mikaila Akeredolu <makerofappsdotcom@gmail.com>
2020-03-15 01:59:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 // initialize the content of the text editor to some Javascript
-//$("#editor").text(`let box = 'valentine day cards'\n console.log(box);`);
+$("#editor").text(`let box = 'valentine day cards'\n console.log(box);`);
   // initialize the content of the text editor to some Javascript
-$("#editor").text(`function echo(m) {\n\treturn m;\n}\nconsole.log(echo("Hello World"));`);
+//$("#editor").text(`function echo(m) {\n\treturn m;\n}\nconsole.log(echo("Hello World"));`);
 
 // initialize the editor environment using the ace library
 var editor = ace.edit("editor");
